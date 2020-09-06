f55bcda5afbd8d77b1e13f9336523ac503d6fe39
Dmitrii Pashutskii <dpashutskii@pm.me>
2020-09-06 14:41:48 +0800

    separate components, add submission

---
+++ src/Form.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Graphs from "./Graphs";
+import "./Form.css";
+
+function Form() {
+  const [textField, setTextField] = useState("");
+  const [showGraph, setShowGraph] = useState(false);
+
+  const handleTextFieldChange = (e) => {
+    setTextField(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowGraph(true);
+  };
+
+  if (showGraph) {
+    return <Graphs value={textField} />;
+  } else {
+    return (
+      <main>
+        <form name="text-form" className="Form-main">
+          <label htmlFor="corpus-input">Insert your text</label>
+          <textarea
+            placeholder="Insert your corpus"
+            className="Form-text-area"
+            name="corpus-input"
+            id="corpus-input"
+            cols="70"
+            rows="10"
+            onChange={handleTextFieldChange}
+            value={textField}
+          ></textarea>
+          <button
+            for="text-form"
+            className="Form-submit"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </main>
+    );
+  }
+}
+
+export default Form;
---
+++ src/Graphs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Graphs(props) {
+  return (
+    <main>You submitteed this text "{props.value}"</main>
+  )
+}
+
+export default Graphs;
